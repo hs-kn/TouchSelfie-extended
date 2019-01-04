4d314a28a896952a3fe3e7c60afaea4f32d2eaee
hs-kn <hannes.schwarz@gmail.com>
2019-01-04 13:34:33 +0100

    added script to install packages

---
+++ install-packages.sh
@@ -0,0 +1,18 @@
+# update system
+sudo apt-get update
+
+# Install ImageTk, Image from PIL
+sudo apt-get install python-imaging
+sudo apt-get install python-imaging-tk
+
+# Install google data api and upgrade it
+sudo apt-get install python-gdata
+sudo pip install --upgrade google-api-python-client
+sudo pip install --upgrade oauth2client
+
+# Install ImageMagick for the 'Animation' mode
+sudo apt-get install imagemagick
+
+# Install CUPS for the Printing function(optional)
+sudo apt-get install cups
+sudo apt-get install python-cups
